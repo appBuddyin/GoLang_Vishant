5234fb32e6b4bc6da0b93ce856fb51f78d1b9dc9
vishant_rana <vshntrn@gmail.com>
2021-08-14 00:06:13 +0530

    add runtime.GOOS

--- goEnv.go
+++ goEnv.go
@@ -11,5 +11,6 @@ func main() {
 	fmt.Println("the compiler is ", runtime.Version())
 	fmt.Println("number of cpu ", runtime.NumCPU())
 	fmt.Println("number of goroutines ", runtime.NumGoroutine())
+	fmt.Println("the operating system is ", runtime.GOOS)
 
 }
