86a7aca0e9ec3998c5f4e41fc75f354ac7732928
vishant_rana <vshntrn@gmail.com>
2021-08-14 00:01:02 +0530

    get environment details

---
+++ goEnv.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	fmt.Println("the compiler is ", runtime.Compiler)
+	fmt.Println(runtime.GOARCH, "machine")
+	fmt.Println("the compiler is ", runtime.Version())
+	fmt.Println("number of cpu ", runtime.NumCPU())
+	fmt.Println("number of goroutines ", runtime.NumGoroutine())
+
+}
