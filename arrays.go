4e97318c90b7f97bb0f012b7cb07692e1a39536c
vishant_rana <vshntrn@gmail.com>
2021-08-14 13:57:25 +0530

    multi-dimentional array

---
+++ arrays.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	anArray := [4]int{1, 2, 3, 4}
+	twoD := [4][4]int{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}}
+	threeD := [2][2][2]int{{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}}
+	fmt.Println("the length of anArray", len(anArray))
+	fmt.Println("the length of threeD", len(threeD))
+	fmt.Println("first element of twoD", twoD[0][0])
+
+	for i := 0; i < len(threeD); i++ {
+		v := threeD[i]
+		for j := 0; j < len(v); j++ {
+			m := v[j]
+			for k := 0; k < len(m); k++ {
+				fmt.Print(m[k], " ")
+			}
+		}
+		fmt.Println()
+
+	}
+}
