527cd423cde84f762984bcefee26dda6c8380bbd
vishant-rana <vshntrn@gmail.com>
2021-08-25 13:10:06 +0530

    calculator api

--- calculator.go
+++ calculator.go
@@ -1,73 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"strconv"
-	"time"
+
+	"calculator/pkg"
 
 	"github.com/gorilla/mux"
 )
 
-type Operation interface {
-	op() float32
-}
-
-type variables struct {
-	var1 float32
-	var2 float32
-}
-
-func (v variables) mul() float32 {
-	return v.var1 * v.var2
-}
-func (v variables) add() float32 {
-	return v.var1 + v.var2
-}
-func (v variables) sub() float32 {
-	return v.var1 - v.var2
-}
-func (v variables) dev() float32 {
-	return v.var1 / v.var2
-}
-
-func calculate(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	w.WriteHeader(http.StatusOK)
-	operand1, _ := strconv.Atoi(vars["var1"])
-	operand2, _ := strconv.Atoi(vars["var2"])
-	operands := &variables{
-		var1: float32(operand1),
-		var2: float32(operand2),
-	}
-
-	var result float32
-	switch vars["operation"] {
-	case "add":
-		result = operands.add()
-	case "mul":
-		result = operands.mul()
-	case "sub":
-		result = operands.sub()
-	case "dev":
-		result = operands.dev()
-	}
-	fmt.Fprintf(w, "Category is: %f\n", result)
-
-	// fmt.Fprintf(w, "Category is: %v\n", vars["operation"])
-	// fmt.Fprintf(w, "var1 is: %v\n", vars["var1"])
-	// fmt.Fprintf(w, "var2 is: %v\n", vars["var2"])
-}
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/calculator/{operation}/{var1:[0-9]+}/{var2:[0-9]+}", ArticleHandler)
+	r.HandleFunc("/calculator/{operation}/{var1:[0-9]+}/{var2:[0-9]+}", pkg.Calculate)
+
 	srv := &http.Server{
-		Handler:      r,
-		Addr:         "127.0.0.1:8000",
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
+		Handler: r,
+		Addr:    "127.0.0.1:8000",
 	}
 	log.Fatal(srv.ListenAndServe())
 }
